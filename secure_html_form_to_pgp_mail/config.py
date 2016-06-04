d03d773ec3b76a55d9f86d9c48168c9b7f008e3f
Steffen Prince <steffen@sprin.io>
2016-06-04 18:06:48 -0500

    Add comments to default config

--- secure_html_form_to_pgp_mail/config.py
+++ secure_html_form_to_pgp_mail/config.py
@@ -7,10 +7,11 @@ import logging
 LOGLEVEL = logging.INFO
 
 #: List of parameters which should not be logged. This should list any
-#: configuration parameters which include secrets, such as SECRET_KEY,
-#: or the database connection string which may have a password.
+#: configuration parameters which include secrets, such as SECRET_KEY.
 DO_NOT_LOG_VARS = [
     'SECRET_KEY',
     'MAIL_PASSWORD',
 ]
+
+#: Default title of emails sent by backend.
 MAIL_TITLE = 'PGP Message'
